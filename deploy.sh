11bacb707c547a47a962e035720fc489c25338b7
adyismad <sandeepagrawal2418@gmail.com>
2021-12-13 22:43:04 +0530

    added deployment files

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,21 @@
 #!/usr/bin/env sh
+
 # abort on errors
 set -e
+
 # build
 npm run build
+
 # navigate into the build output directory
 cd dist
+
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
+
 git init
 git add -A
 git commit -m 'deploy'
-git push -f sagrawal2418@github.com:sagrawal2418/cs601_project master:gh-pages
+
+git push -f git@github.com:sagrawal2418/cs601_project.git main:gh-pages
+
 cd -
